32113dd541ecda653dba2759d3ee39ac70c17081
Haroon Ahmad <ranaharoonahmad5566@gmail.com>
2025-08-04 02:35:53 +0500

    Add files via upload

---
+++ About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import noteContext from '../context/notes/noteContext'
+const About = () => {
+  // const a = useContext(noteContext)
+ 
+  return (
+    <div>
+      {/* This is About {a.name} and he is in the class {a.class} */}
+      This is About page
+    </div>
+  )
+}
+
+export default About
---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/inotebook');
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Could not connect to MongoDB", error);
+    }
+};
+
+module.exports = connectToMongo;
